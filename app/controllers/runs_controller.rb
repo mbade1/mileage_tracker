526dc14facacd7962f938f3a5ef1181bf3291d61
mbade1 <mbade314@gmail.com>
2020-07-03 13:57:18 -0500

    Add mileage

--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -2,26 +2,18 @@ class RunsController < ApplicationController
 
     get '/shoes/:id/runs' do
         @shoe = Shoe.find(params[:id])
-        binding.pry
         erb :'runs/runs'
     end
 
     patch '/runs/:id' do
         @shoe = Shoe.find(params[:id])
-        @shoe.name = params[:name]
-        @shoe.date = params[:date]
-        @shoe.new_mileage = params[:new_mileage]
-        @shoe.current_mileage = params[:current_mileage].update_mileage
-        erb :'/runs/runs'
-    end
-
-    helpers do
-        def update_mileage
-            @shoe.current_mileage = params[:current_mileage]
-            input = params[:latest_run]
-            updated_mileage = @shoe.current_mileage - input
-            @shoe.current_mileage = updated_mileage
-            @shoe.current_mileage
+        @shoe.latest_run = params[:latest_run]
+        @newest_run = (@shoe.current_mileage.to_i + @shoe.latest_run.to_i)
+        @shoe.current_mileage = @newest_run
+        if !@shoe.save
+            erb :'runs/runs'
+        else
+            redirect to("/shoes/#{@shoe.id}")
         end
     end
 end
