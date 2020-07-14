ec1bef9da9d3a1bd1f3e0c041da87651b7b357d0
mbade1 <mbade314@gmail.com>
2020-07-14 17:26:07 -0500

    Cleaned up code

--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -7,19 +7,26 @@ class RunsController < ApplicationController
 
   post '/shoes/:id' do
     @shoe = Shoe.find(params[:id])
-    @runs = Run.create
+    @runs = Run.new
     @runs.date = params[:date]
     @runs.location = params[:location]
     @runs.miles_run = params[:miles_run]
     @runs.shoe_id = @shoe.id
+    if @runs.miles_run.to_i <= 0
+      redirect to("/shoes/#{@shoe.id}")
+    end
     if logged_in?
       @newest_run = (@shoe.current_mileage.to_i + @runs.miles_run.to_i)
       @shoe.current_mileage = @newest_run
-      @runs.save
-      flash[:message] = "A NEW RUN HAS BEEN LOGGED"
-      redirect to("/shoes/#{@shoe.id}")
+      if !@runs.save
+        redirect to("/shoes/#{@shoe.id}")
+      else
+        flash[:message] = "A NEW RUN HAS BEEN LOGGED!"
+        redirect to("/shoes/#{@shoe.id}")
+      end
     else
       redirect to("/login")
     end
   end
 end
+
